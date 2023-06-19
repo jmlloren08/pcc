29be768463912b7bd2656c1f9108ca5000894213
jmlloren08 <jaymarvinc.lloren@gmail.com>
2023-06-19 15:49:57 +0800

    1

---
+++ index.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PCC - College of Law</title>
+    <link rel="stylesheet" href="dist/css/bootstrap.css">
+    <link rel="stylesheet" href="dist/css/styles.css">
+</head>
+
+<body>
+
+    <section class="section-nav">
+        <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+            <div class="container-fluid d-flex">
+                <div class="zoom p-2 flex-grow-1"><a class="navbar-brand" href="./">
+                        <img src="img/navbar/pcc_logo2.png" alt="pcc-logo" class="imgNavbarLogo">
+                        Pagadian Capitol College
+                    </a>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse justify-content-md-center collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item  p-2 zoom">
+                                <a class="nav-link active" area-current="page" href="./">Home</a>
+                            </li>
+                            <li class="nav-item p-2 zoom">
+                                <a class="nav-link" href="about-us">About us</a>
+                            </li>
+                            <li class="nav-item p-2 zoom">
+                                <a class="nav-link" href="programs">Programs</a>
+                            </li>
+                            <li class="nav-item p-2 zoom">
+                                <a class="nav-link" href="faculty">Faculty</a>
+                            </li>
+                            <li class="nav-item p-2 zoom">
+                                <a class="nav-link" href="admissions">Admissions</a>
+                            </li>
+                            <li class="nav-item p-2 zoom">
+                                <a class="nav-link" href="events">Events</a>
+                            </li>
+                            <li class="nav-item p-2 zoom">
+                                <a class="nav-link" href="resources">Resources</a>
+                            </li>
+                        </ul>
+
+                        <ul class="navbar-nav action">
+                            <li class="nav-item p-2">
+                                <a href="contact" class="btn ml-lg-auto btn-bordered-white"><i
+                                        class="fas fa-paper-plane contact-icon mr-md-2"></i>Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <section>
+        <div id="pcc-carousel" class="carousel slide" data-bs-ride="carousel">
+
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#pcc-carousel" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#pcc-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#pcc-carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+
+            <div class="carousel-inner">
+                <div class="carousel-item active c-item">
+                    <img src="img/slider/Slide_1.jpg" class="imgCarousel d-block w-100" alt="Slide 1">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Pagadian Capitol College</h5>
+                        <p>Enjoy a Top-Notch Education
+                            Taking the Lead in Providing Quality Education</p>
+                    </div>
+                </div>
+                <div class="carousel-item c-item">
+                    <img src="img/slider/Slide_2.jpg" class="imgCarousel d-block w-100" alt="Slide 2">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Pagadian Capitol College</h5>
+                        <p>Enjoy a Top-Notch Education
+                            Taking the Lead in Providing Quality Education</p>
+                    </div>
+                </div>
+                <div class="carousel-item c-item">
+                    <img src="img/slider/Slide_3.jpg" class="imgCarousel d-block w-100" alt="Slide 3">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Pagadian Capitol College</h5>
+                        <p>Enjoy a Top-Notch Education
+                            Taking the Lead in Providing Quality Education</p>
+                    </div>
+                </div>
+            </div>
+            
+            <button class="carousel-control-prev" type="button" data-bs-target="#pcc-carousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#pcc-carousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </section>
+
+    <section class="section">
+        <div class="container">
+            <div class="row">
+
+            </div>
+        </div>
+    </section>
+
+    <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center border-top">
+            <div class="col-md-4 d-flex align-items-center">
+                <a href="./" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                    <img src="img/navbar/pcc_logo2.png" alt="pcc-logo" class="imgFooterLogo">
+                </a>
+                <span class="mb-3 mb-md-0 text-body-secondary">&copy;
+                    <script>document.write(new Date().getFullYear());</script> Pagadian Capitol College - College of
+                    Law.
+                </span>
+            </div>
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                <li class="ms-3">
+                    <a href="twitter.com/pcc-law" class="text-body-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                            viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                            <path
+                                d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
+                        </svg>
+                    </a>
+                </li>
+                <li class="ms-3">
+                    <a href="instagram.com/pcc-law" class="text-body-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                            viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                            <path
+                                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+                        </svg>
+                    </a>
+                </li>
+                <li class="ms-3">
+                    <a href="facebook.com/pcc-law" class="text-body-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                            viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                            <path
+                                d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z" />
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </footer>
+    </div>
+
+    <script src="dist/js/plugins.min.js"></script>
+    <script src="dist/js/bootstrap.bundle.js"></script>
+
+</body>
+
+</html>
