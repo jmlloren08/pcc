4ea2d7008ed05a7ac3b24bd136bd94ef824b29c9
jmlloren08 <jaymarvinc.lloren@gmail.com>
2023-06-27 11:59:49 +0800

    1

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
 <body>
     <header class="section-nav">
         <section class="section-nav">
-            <nav class="navbar navbar-expand-lg fixed-top navbar-bg p-0">
+            <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark px-5 py-2">
                 <div class="container-fluid d-flex">
-                    <div class="zoom p-2"><a class="navbar-brand" href="./">
+                    <div class="zoom"><a class="navbar-brand" href="./">
                             <img src="img/navbar/navbarlogo.png">
                         </a>
                     </div>
@@ -114,8 +114,15 @@
                             <h3>ADMISSION</h3>
                         </div>
                         <div class="res-margin">
-                            <p> The process of getting into Pagadian City College - College of Law is designed to ensure that qualified and motivated individuals have the opportunity to pursue their legal education.</p>
-                            <a class="readmore-btn mt-3 color-1" href="admissiondetails">READ MORE</a> <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+                            <p> The process of getting into Pagadian City College - College of Law is designed to ensure
+                                that qualified and motivated individuals have the opportunity to pursue their legal
+                                education.</p>
+                            <a class="readmore-btn mt-3 color-1" href="admissiondetails">READ MORE</a> <svg
+                                xmlns="http://www.w3.org/2000/svg" height="1em"
+                                viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <path
+                                    d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+                            </svg>
                         </div>
                     </div>
                 </div>
@@ -125,8 +132,14 @@
                             <h3>SCHOLARSHIP</h3>
                         </div>
                         <div class="res-margin">
-                            <p>PCC - College of Law offers various scholarships and financial aid opportunities to support students in their pursuit of a legal education.</p>
-                            <a class="readmore-btn mt-3 color-1" href="scholarshipdetails">READ MORE</a> <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+                            <p>PCC - College of Law offers various scholarships and financial aid opportunities to
+                                support students in their pursuit of a legal education.</p>
+                            <a class="readmore-btn mt-3 color-1" href="scholarshipdetails">READ MORE</a> <svg
+                                xmlns="http://www.w3.org/2000/svg" height="1em"
+                                viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <path
+                                    d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+                            </svg>
                         </div>
                     </div>
                 </div>
@@ -136,8 +149,15 @@
                             <h3>ACADEMIC PROGRAMS</h3>
                         </div>
                         <div class="res-margin">
-                            <p>PCC - College of Law offers a range of academic programs designed to equip students with the knowledge, skills, and ethical foundation necessary for a successful legal career. </p>
-                            <a class="readmore-btn mt-3 color-1" href="academicdetails">READ MORE</a> <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+                            <p>PCC - College of Law offers a range of academic programs designed to equip students with
+                                the knowledge, skills, and ethical foundation necessary for a successful legal career.
+                            </p>
+                            <a class="readmore-btn mt-3 color-1" href="academicdetails">READ MORE</a> <svg
+                                xmlns="http://www.w3.org/2000/svg" height="1em"
+                                viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <path
+                                    d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+                            </svg>
                         </div>
                     </div>
                 </div>
@@ -150,9 +170,16 @@
             <div class="row justify-content-center">
                 <div class="col-12 col-lg-10">
                     <div class="section-heading text-center m-0">
-                        <h2 class="text-white">The College of Law at Pagadian City College has a rich history and a strong reputation for providing quality legal education in the region.</h2>
-                        <p class="text-white d-none d-sm-block mt-4">Here is an overview of our college, highlighting its history, accreditation, affiliations, notable achievements, and distinguishing factors.</p>
-                        <a href="aboutusdetails" class="btn btn-bordered-white mt-4"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg> Explore PCC</a>
+                        <h2 class="text-white">The College of Law at Pagadian City College has a rich history and a
+                            strong reputation for providing quality legal education in the region.</h2>
+                        <p class="text-white d-sm-block">Here is an overview of our college, highlighting its history,
+                            accreditation, affiliations, notable achievements, and distinguishing factors.</p>
+                        <a href="aboutusdetails" class="btn btn-bordered-white"><svg xmlns="http://www.w3.org/2000/svg"
+                                height="1em"
+                                viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <path
+                                    d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                            </svg> Explore PCC</a>
                     </div>
                 </div>
             </div>
@@ -165,8 +192,8 @@
                 <div class="col-12 col-md-10 col-lg-7">
                     <div class="section-heading text-center">
                         <h2 class="text-blue">ACADEMIC PROGRAMS</h2>
-                        <p class="d-none d-sm-block mt-4">Slides play here...</p>
-                        <a href="academicprograms" class="btn btn-bordered-blue mt-4">Choose your programs</a>
+                        <p class="d-sm-block">Slides play here...</p>
+                        <a href="academicprograms" class="btn btn-bordered-blue">Choose your programs</a>
                     </div>
                 </div>
             </div>
@@ -179,10 +206,48 @@
                 <div class="col-12 col-md-10 col-lg-7">
                     <div class="section-heading text-center">
                         <h2 class="text-grey">SET APPOINTMENT DATE</h2>
-                        <p class="d-none d-sm-block mt-4">To set an appointment online with PCC - College of Law, you may click the button below.</p>
+                        <p class="d-sm-block">To set an appointment online with PCC - College of Law, you may click the
+                            button below.</p>
                         <a href="setappdate" class="btn btn-bordered-grey text-grey">Choose Date</a>
                     </div>
                 </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section">
+        <div class="row justify-content-center">
+            <div class="column1">
+                <div class="col-11">
+                    <div class="text-center">
+                        <h2 class="text-white">INQUIRE NOW</h2>
+                        <p class="text-white">We'd love to hear from you</p>
+                    </div>
+                    <div class="contact-us">
+                        <ul>
+                            <li class="contact-info color-1 bg-hover active hover-bottom text-center p-5 m-3">
+                                <span><svg xmlns="http://www.w3.org/2000/svg" height="2em"
+                                        viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                        <path
+                                            d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64H80V384H304V64z" />
+                                    </svg></span>
+                                <h3 class="text-white d-block d-sm-block my-2">+639204050015</h3>
+                            </li>
+                            <li class="contact-info color-2 bg-hover active hover-bottom text-center p-5 m-3">
+                                <span><svg xmlns="http://www.w3.org/2000/svg" height="2em"
+                                        viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                        <path
+                                            d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
+                                    </svg></span>
+                                <h3 class="text-white d-block d-sm-block my-2">pcclaw@gmail.com</h3>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="column2">
+                <h2>Column 2</h2>
+                <p>Details</p>
             </div>
         </div>
     </section>
